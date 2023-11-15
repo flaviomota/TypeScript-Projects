41cdeae146aecce6e4e6a8ca08a1ac595b184066
flaviomota <flaviofilipemota@gmail.com>
2023-11-15 11:48:45 +0000

    Stats: ConsoleReport | Analysis

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,24 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
-import { CsvFileReader } from './CsvFileReader';
+import { Summary } from './Summary';
 
-// Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
-// Create an instance of MatchReader and pass in something satisfying
-// the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-// matchReader.matches
-
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games this season.`);
+summary.buildAndPrintReport(matchReader.matches);
