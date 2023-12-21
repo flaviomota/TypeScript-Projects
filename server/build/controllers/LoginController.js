ebd1f051a7f3b0112191c355090e5b82a583b439
flaviomota <flaviofilipemota@gmail.com>
2023-12-21 16:21:46 +0100

    Server: refactoring into RootController.ts

--- server/build/controllers/LoginController.js
+++ server/build/controllers/LoginController.js
@@ -16,12 +16,42 @@ var LoginController = /** @class */ (function () {
     LoginController.prototype.getLogin = function (req, res) {
         res.send("\n\t\t<form method=\"POST\">\n\t\t\t<div>\n\t\t\t\t<label>Email</label>\n\t\t\t\t<input name=\"email\" />\n\t\t\t</div>\n\t\t\t<div>\n\t\t\t\t<label>Password</label>\n\t\t\t\t<input name=\"password\" type=\"password\" />\n\t\t\t</div>\n\t\t\t<button>Submit</button>\n\t\t</from>\n\t");
     };
+    LoginController.prototype.postLogion = function (_a) {
+        var req = _a.req, res = _a.res;
+        var _b = req.body, email = _b.email, password = _b.password;
+        if (email && password && email === 'hi@hi.com' && password === 'password') {
+            // mark this user as logged in
+            req.session = { loggedIn: true };
+            // redirect them to the root route
+            res.redirect('/');
+        }
+        else {
+            res.send('Invalid email or password!');
+        }
+    };
+    LoginController.prototype.getLogout = function (req, res) {
+        req.session = undefined;
+        res.redirect('/');
+    };
     __decorate([
         (0, decorators_1.get)('/login'),
         __metadata("design:type", Function),
         __metadata("design:paramtypes", [Object, Object]),
         __metadata("design:returntype", void 0)
     ], LoginController.prototype, "getLogin", null);
+    __decorate([
+        (0, decorators_1.post)('./login'),
+        (0, decorators_1.bodyValidator)('email', 'password'),
+        __metadata("design:type", Function),
+        __metadata("design:paramtypes", [Object]),
+        __metadata("design:returntype", void 0)
+    ], LoginController.prototype, "postLogion", null);
+    __decorate([
+        (0, decorators_1.get)('/logout'),
+        __metadata("design:type", Function),
+        __metadata("design:paramtypes", [Object, Object]),
+        __metadata("design:returntype", void 0)
+    ], LoginController.prototype, "getLogout", null);
     LoginController = __decorate([
         (0, decorators_1.controller)('/auth')
     ], LoginController);
--- server/build/routes/loginRoutes.js
+++ server/build/routes/loginRoutes.js
@@ -2,40 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
-function requireAuth(req, res, next) {
-    if (req.session && req.session.loggedIn) {
-        next();
-        return;
-    }
-    res.status(403);
-    res.send('Not permitted!');
-}
 var router = (0, express_1.Router)();
 exports.router = router;
-router.post('/login', function (req, res) {
-    var _a = req.body, email = _a.email, password = _a.password;
-    if (email && password && email === 'hi@hi.com' && password === 'password') {
-        // mark this user as logged in
-        req.session = { loggedIn: true };
-        // redirect them to the root route
-        res.redirect('/');
-    }
-    else {
-        res.send('Invalid email or password!');
-    }
-});
-router.get('/', function (req, res) {
-    if (req.session && req.session.loggedIn) {
-        res.send("\n\t\t\t<div>\n\t\t\t\t<div>You are logged in!</div>\n\t\t\t\t<a href=\"/logout\">Logout</a>\n\t\t\t</div>\n\t\t");
-    }
-    else {
-        res.send("\n\t\t<div>\n\t\t\t<div>You are not logged in!</div>\n\t\t\t<a href=\"/login\">Log In</a>\n\t\t</div>\n\t");
-    }
-});
-router.get('/logout', function (req, res) {
-    req.session = undefined;
-    res.redirect('/');
-});
-router.get('/protected', requireAuth, function (req, res) {
-    res.send('Welcome to protected route, logged in user');
-});
